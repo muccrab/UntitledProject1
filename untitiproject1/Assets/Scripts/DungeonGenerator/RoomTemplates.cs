b6c4157738a419ff8d9b54029c5d3d8df3294cfc
darker1221sk <dominitator@gmail.com>
2022-06-25 19:02:42 +0200

    Dungeon generator 1.1
    stale nejde vyhadzuje roory na 2 riadkoch

--- untitiproject1/Assets/Scripts/DungeonGenerator/RoomTemplates.cs
+++ untitiproject1/Assets/Scripts/DungeonGenerator/RoomTemplates.cs
@@ -4,10 +4,37 @@ using UnityEngine;
 
 public class RoomTemplates : MonoBehaviour
 {
-    public GameObject[] bottomRooms;
-    public GameObject[] topRooms;
-    public GameObject[] leftRooms;
-    public GameObject[] rightRooms;
 
-    public GameObject closedRoom;
+	public GameObject[] bottomRooms;  // polia kde sa daju roomky ktore sa budu spawnovat 
+	public GameObject[] topRooms;
+	public GameObject[] leftRooms;
+	public GameObject[] rightRooms;
+
+	public GameObject closedRoom;  //roomka na uzatvorenie miestnosti
+
+	public List<GameObject> rooms; //list kde sa pridavaju roomky ktore sa spawnu
+
+	public float waitTime;
+	private bool spawnedBoss;
+	public GameObject boss;
+
+	void Update()
+	{
+
+		if (waitTime <= 0 && spawnedBoss == false)
+		{
+			for (int i = 0; i < rooms.Count; i++)
+			{
+				if (i == rooms.Count - 1) // spawne bossa ked pocet roomiek sa naplni
+				{
+					Instantiate(boss, rooms[i].transform.position, Quaternion.identity);
+					spawnedBoss = true;
+				}
+			}
+		}
+		else
+		{
+			waitTime -= Time.deltaTime;
+		}
+	}
 }
