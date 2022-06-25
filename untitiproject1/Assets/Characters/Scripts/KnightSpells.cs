c8bf29fcb3d6c572ea42643096393fef65e0bdf0
Eras0 <tanevskyp78@gmail.com>
2022-06-25 14:25:39 +0200

    ifsatementjekokotinapsia
    jebemtiif

--- untitiproject1/Assets/Characters/Scripts/KnightSpells.cs
+++ untitiproject1/Assets/Characters/Scripts/KnightSpells.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,14 +9,26 @@ public class KnightSpells : MonoBehaviour
 {
     public Sprite[] spellIcons;
     public GameObject[] positions;
+    public bool[] target = { false, false, false, false };
+    public bool[] targetable = { false, false, false, false };
 
     private int CurrentSpell = 0;
 
     private bool enemyFound = false;
 
+    Vector3 mousePos;
+    Vector2 mousePos2D;
+    private void Start()
+    {
+
+    }
+
     private void Update()
     {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos2D = new Vector2(mousePos.x, mousePos.y);
         spell1use();
+        EnemyPointedAt();
     }
 
     
@@ -24,6 +37,7 @@ public class KnightSpells : MonoBehaviour
     public void spell1()
     {
         CurrentSpell = 1;
+        SetTargetable(0,3);
     }
 
     private void spell1use()
@@ -36,11 +50,8 @@ public class KnightSpells : MonoBehaviour
                 CurrentSpell = 0;
                 enemyFound = false;
                 positions[pos].GetComponentInChildren<Enemy>().health -= 20;
-                
+                ResetTargets();
             }
-
-
-
         }
         
     }
@@ -54,8 +65,7 @@ public class KnightSpells : MonoBehaviour
         int pos = 0;
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            
             for (int i = 0; i < 4; i++)
             {
                 if (positions[i].GetComponentInChildren<BoxCollider2D>().bounds.Contains(mousePos2D))
@@ -68,5 +78,45 @@ public class KnightSpells : MonoBehaviour
         return pos;
     }
 
+    public void SetTargetable(int min, int max)
+    {
+        for (int i = min; i < max; i++)
+        {
+            targetable[i] = true;
+            positions[i].transform.Find("TargetablePointer").gameObject.SetActive(true);
+        }
+    }
+
+    private void EnemyPointedAt()
+    {
+      //  Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+      //  Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+        for (int i = 0; i < 4; i++)
+        {
+            if (positions[i].GetComponentInChildren<BoxCollider2D>().bounds.Contains(mousePos2D))
+            {
+                if(targetable[i] == true)
+                {
+                    positions[i].transform.Find("TargetPointer").gameObject.SetActive(true);
+                }
+            }
+            else
+            {
+                positions[i].transform.Find("TargetPointer").gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void ResetTargets()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            targetable[i] = false;
+            target[i] = false;
+            positions[i].transform.Find("TargetablePointer").gameObject.SetActive(false);
+            positions[i].transform.Find("TargetPointer").gameObject.SetActive(false);
+        }
+    }
+    
 }
 
