21374e0adc5c32e58b9ec1e276dd62ae1ce30b74
muccrab <muccrab.2002@gmail.com>
2022-06-29 20:10:24 +0200

    Zivot je Life

---
+++ untitiproject1/Assets/Scripts/DungeonGenerator/DunGen.cs
@@ -0,0 +1,143 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DunGen : MonoBehaviour
+{
+    
+    public GameObject Tile;
+    public Vector2Int mainRange;
+    public int Radius;
+    int mainHops=0;
+    int tileLength = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Random.InitState((int)Time.time); //sets seed for Generation
+        tileLength = (int)Tile.GetComponent<SpriteRenderer>().bounds.size.x; //Obtains the Length of Tile
+        mainHops = Random.Range(mainRange.x+0,mainRange.y+1); //Gets Hops of Main Road
+
+
+
+        //Array of main Road stops
+        TurnPoint[] mainPoints = new TurnPoint[mainHops];
+        for(int i = mainHops-1; i >= 0; i--)
+        {
+            if (i != mainHops-1)
+            {
+                mainPoints[mainHops-i-1] = new TurnPoint(mainPoints[mainHops-2-i].nextHop,i,true,Radius,tileLength);
+                //Debug.Log(mainPoints[mainHops-i-1].nextHop.x);
+            }
+            else
+            {
+                mainPoints[mainHops-i-1] = new TurnPoint(new Vector2Int(0,0),i,true,Radius,tileLength); 
+            }
+        }
+
+
+        Debug.Log(mainHops);
+        for (int j = 0; j < mainHops; j++)
+        {
+            TurnPoint tp = mainPoints[j];
+            GameObject tile = Instantiate(Tile);
+            tile.name = "Room "+j;
+            tile.transform.position = new Vector2(tp.position.x,tp.position.y);
+
+            if (j!=mainHops-1)
+            {
+                TurnPoint ntp = mainPoints[j+1];
+                int tilesBetweenLength = (int)Mathf.Abs((ntp.position.x-tp.position.x)/tileLength)+(int)Mathf.Abs((ntp.position.y-tp.position.y)/tileLength)+1;
+                Vector2[] tilesBetween = new Vector2[100];
+                tilesBetween[0].y = tp.position.y;
+                //Debug.Log(tp.position.x +" - "+ ntp.position.x);
+                //Debug.Log(Mathf.Abs((tp.position.x-ntp.position.x))+" / "+ tileLength+" + "+ Mathf.Abs((tp.position.y-ntp.position.y)));
+                Debug.Log("Tiles Between - "+tilesBetweenLength);
+                int tIndex = 0;
+                
+                for(int i = tp.position.x; nextPointCheck(i,tp.position.x,ntp.position.x); i+=(int)Mathf.Sign(ntp.position.x-tp.position.x)*tileLength)
+                {
+                    //Debug.Log(i);
+                    if (tIndex==0) tilesBetween[0].y = tp.position.y;
+                    else tilesBetween[tIndex].y = tilesBetween[tIndex-1].y;
+                    tilesBetween[tIndex].x = i;
+                    tIndex++;
+                    //if (tIndex>100) break;
+                    Debug.Log("last position - "+tp.position.x+" - position - "+i+" - next position - "+ntp.position.x);
+                }
+              
+                //tIndex--;
+               
+                
+                for(int i = tp.position.y; nextPointCheck(i,tp.position.y,ntp.position.y); i+=(int)Mathf.Sign(ntp.position.y-tp.position.y)*tileLength)
+                {
+                    //Debug.Log(i);
+                    if (tIndex==0) tilesBetween[tIndex].x = tp.position.x;
+                    else tilesBetween[tIndex].x = tilesBetween[tIndex-1].x;
+                    tilesBetween[tIndex].y = i;
+                    tIndex++;
+                    //if (tIndex>100) break;
+                }
+                for(int i = 0; i < tilesBetweenLength; i++)
+                {
+                    GameObject tilebetween = Instantiate(Tile);
+                    tilebetween.name = "Hall "+j;
+                    tilebetween.transform.position = new Vector2(tilesBetween[i].x,tilesBetween[i].y);
+                }
+            }
+        }
+
+    }
+
+
+    bool nextPointCheck(int pos, int lastpos, int nextpos)
+    {
+        if (nextpos==lastpos)return false;
+        if (lastpos<nextpos)
+        {
+            if (pos <= nextpos) return true;
+            return false;
+        }
+        if (pos >= nextpos) return true;
+            return false;
+    }
+
+}
+
+class TurnPoint{
+
+    public Vector2Int position;
+    public Vector2Int nextHop;
+    public int hopCount;
+    public int branchcount = 0;
+    int Radius;
+    int tileLength;
+
+    public TurnPoint(Vector2Int position,int hopCount, bool isMain, int Radius, int tileLength){
+        this.position.x = position.x;
+        this.position.y = position.y;
+        this.hopCount = hopCount;
+        this.Radius = Radius;
+        this.tileLength = tileLength;
+        if (isMain)
+        {
+            branchcount = Random.Range(0,3+1);
+        }
+        if(hopCount>0)
+        {
+            nextHop = this.newPoint();
+        }
+    }
+    Vector2Int newPoint(){
+        Vector2Int nextHopP = new Vector2Int();
+
+        //Gets left corner of finishing tile
+        nextHopP.x = Random.Range(position.x-Radius,position.x+Radius); 
+        nextHopP.x = nextHopP.x - (nextHopP.x % tileLength); 
+        
+        nextHopP.y = Random.Range(position.y-Radius,position.y+Radius);
+        nextHopP.y = nextHopP.y - (nextHopP.y % tileLength);
+        Debug.Log("x = "+nextHopP.x + "y = "+nextHopP.y);
+        return nextHopP;
+    }
+}
