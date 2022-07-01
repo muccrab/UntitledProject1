7864f8dee335a8cbc66998b9124ef4f1d8b772e3
iBRUHprofen <98183542+iBRUHprofen@users.noreply.github.com>
2022-07-01 19:19:20 +0200

    Buttons

--- untitiproject1/Assets/Dedina/Skripty/CharActionButtons.cs
+++ untitiproject1/Assets/Dedina/Skripty/CharActionButtons.cs
@@ -14,6 +14,19 @@ public class CharActionButtons : MonoBehaviour
     public SpriteRenderer mutationTreatmentHolder;
 
 
+
+    public SpriteRenderer drinkHolder;
+    public SpriteRenderer danceHolder;
+    public SpriteRenderer eatHolder;
+
+
+    public SpriteRenderer prayHolder;
+    public SpriteRenderer confessHolder;
+    public SpriteRenderer somethingHolder;
+
+
+
+
     public void mutation()
     {
        int arrayPosition = Convert.ToInt32(charActionMenu.buttonName) - 1;
@@ -22,5 +35,60 @@ public class CharActionButtons : MonoBehaviour
 
        mutationTreatmentHolder.sprite = positionSprites[arrayPosition];
     }
+
+    public void drink()
+    {
+       int arrayPosition = Convert.ToInt32(charActionMenu.buttonName) - 1;
+
+       Debug.Log(arrayPosition);
+
+       drinkHolder.sprite = positionSprites[arrayPosition];
+    }
+
+    public void dance()
+    {
+       int arrayPosition = Convert.ToInt32(charActionMenu.buttonName) - 1;
+
+       Debug.Log(arrayPosition);
+
+       danceHolder.sprite = positionSprites[arrayPosition];
+    }
+
+    public void eat()
+    {
+       int arrayPosition = Convert.ToInt32(charActionMenu.buttonName) - 1;
+
+       Debug.Log(arrayPosition);
+
+       eatHolder.sprite = positionSprites[arrayPosition];
+    }
+
+    public void pray()
+    {
+       int arrayPosition = Convert.ToInt32(charActionMenu.buttonName) - 1;
+
+       Debug.Log(arrayPosition);
+
+       prayHolder.sprite = positionSprites[arrayPosition];
+    }
+
+    public void confess()
+    {
+       int arrayPosition = Convert.ToInt32(charActionMenu.buttonName) - 1;
+
+       Debug.Log(arrayPosition);
+
+       confessHolder.sprite = positionSprites[arrayPosition];
+    }
+
+    public void something()
+    {
+       int arrayPosition = Convert.ToInt32(charActionMenu.buttonName) - 1;
+
+       Debug.Log(arrayPosition);
+
+       somethingHolder.sprite = positionSprites[arrayPosition];
+    }
+
 
 }
